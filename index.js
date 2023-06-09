2372b129c7fc4ad56ac972b210bfba1cd83a6275
Ha Thanh <Hai>
2023-06-09 19:56:49 +0700

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send(`<h1>Hà Thanh Hải - Tèo em ơi</h1>`)
+  res.send(`<h1>Hà Thanh Hải - Tèo em ơi con mèo treo cây cao</h1>`)
 })
 
 app.listen(port, () => {
